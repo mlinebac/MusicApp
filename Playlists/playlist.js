d12297064adb054deed697dfe4a5259bdc550260
Charles Williams <charlesjw94@outlook.com>
2016-04-12 19:52:57 -0400

    music player functioning

--- Playlists/playlist.js
+++ Playlists/playlist.js
@@ -1,66 +1,80 @@
-var vid, playbtn, seekslider, curtimetext, durtimetext, mutebtn, volumeslider, fullscreenbtn;
+(function () {
+    "use strict";
 
-function intializePlayer() {
-         // Set object references
-         vid = document.getElementById("my_video");
-         playbtn = document.getElementById("playpausebtn");
-         seekslider = document.getElementById("seekslider");
-         curtimetext = document.getElementById("curtimetext");
-         durtimetext = document.getElementById("durtimetext");
-         mutebtn = document.getElementById("mutebtn");
-         volumeslider = document.getElementById("volumeslider");
-         fullscreenbtn = document.getElementById("fullscreenbtn");
-         // Add event listeners
-         playbtn.addEventListener("click",playPause,false);
-         seekslider.addEventListener("change",vidSeek,false);
-         vid.addEventListener("timeupdate",seektimeupdate,false);
-         mutebtn.addEventListener("click",vidmute,false);
-         volumeslider.addEventListener("change",setvolume,false);
-         fullscreenbtnbtn.addEventListener("click",toggleFullScreen,false);
-        }
+    var main = function () {
+        var vid, playbtn, seekslider, curtimetext, durtimetext, mutebtn, volumeslider, fullscreenbtn;
 
-window.onload = intializePlayer;
+        var intializePlayer = function() {
+            // Set object references
+            vid = document.getElementById("my_video");
+            playbtn = document.getElementById("playpausebtn");
+            seekslider = document.getElementById("seekslider");
+            curtimetext = document.getElementById("curtimetext");
+            durtimetext = document.getElementById("durtimetext");
+            mutebtn = document.getElementById("mutebtn");
+            volumeslider = document.getElementById("volumeslider");
+            // Add event listeners
+            playbtn.addEventListener("click", playPause, false);
+            seekslider.addEventListener("change", vidSeek, false);
+            vid.addEventListener("timeupdate", seektimeupdate, false);
+            mutebtn.addEventListener("click", vidmute, false);
+            volumeslider.addEventListener("change", setvolume, false);
+        };
 
-function playPause(){
-            if(vid.paused) {
-             vid.play();
+        window.onload = intializePlayer;
+
+        var playPause = function() {
+            if (vid.paused) {
+                vid.play();
                 playbtn.innerHTML = "&#9612 &#9612";
-            }
-            else {
-              vid.pause();
+            } else {
+                vid.pause();
                 playbtn.innerHTML = "&#9658";
             }
-        }
-function vidSeek(){
-         var seekto = vid.duration * (seekslider.value/100);
-         vid.currentTime = seekto;
-        }
-function seektimeupdate() {
+        };
+
+        var vidSeek = function() {
+            var seekto = vid.duration * (seekslider.value / 100);
+            vid.currentTime = seekto;
+        };
+
+        var seektimeupdate = function() {
             var nt = vid.currentTime * (100 / vid.duration);
             seekslider.value = nt;
             var curmins = Math.floor(vid.currentTime / 60);
             var cursecs = Math.floor(vid.currentTime - curmins * 60);
             var durmins = Math.floor(vid.duration / 60);
             var dursecs = Math.floor(vid.duration - durmins * 60);
-            if(cursecs < 10){ cursecs = "0"+cursecs;   }
-            if(dursecs < 10){ dursecs = "0"+dursecs;   }
-            if(curmins < 10){ curmins = "0"+curmins;   }
-            if(durmins < 10){ durmins = "0"+durmins;   }
-            curtimetext.innerHTML = curmins+":"+cursecs;
-            durtimetext.innerHTML = durmins+":"+dursecs;
-        }
-function vidmute() {
-         if(vid.muted){
-             vid.muted = false;
-             mutebtn.innerHTML = "&#128266";
-         }
-         else {
-             vid.muted = true;
-             mutebtn.innerHTML = "&#128263";
-         }
-        }
-function setvolume() {
+            if (cursecs < 10) {
+                cursecs = "0" + cursecs;
+            }
+            if (dursecs < 10) {
+                dursecs = "0" + dursecs;
+            }
+            if (curmins < 10) {
+                curmins = "0" + curmins;
+            }
+            if (durmins < 10) {
+                durmins = "0" + durmins;
+            }
+            curtimetext.innerHTML = curmins + ":" + cursecs;
+            durtimetext.innerHTML = durmins + ":" + dursecs;
+        };
+
+        var vidmute = function() {
+            if (vid.muted) {
+                vid.muted = false;
+                mutebtn.innerHTML = "&#128266";
+            } else {
+                vid.muted = true;
+                mutebtn.innerHTML = "&#128263";
+            }
+        };
+
+        var setvolume = function() {
             vid.volume = volumeslider.value / 100;
-        }
+        };
+    };
 
-
+    main();
+}());
