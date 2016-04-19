d916291eaf8364a03b0cfb7971e49f65dbd8f6e0
mlinebac <mlinebac@unca.edu>
2016-04-19 10:48:13 -0400

    made angular changes to music.js

--- music.js
+++ music.js
@@ -5,6 +5,19 @@
 		$scope.artist = $scope.title = $scope.path = $scope.likes = $scope.dislikes = '';
 		$scope.playlists = [];
 
+		$scope.all = function(){
+			$.getJSON('getPlaylist', function(result){
+				$scope.playlists = result;
+			});
+		};
+
+		$scope.update = function(){
+			var updateArtist = {"artist" : $scope.artist, "likes" : $scope.likes, "dislikes" : $scope.dislikes};
+			$.post('updateArtist', updateArtist);
+			$scope.artist = $scope.likes = $scope.dislikes = '';
+		};
+
+
 	};
 ]);
 }());
